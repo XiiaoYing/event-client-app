66a01a250030597906aa0faac298ee2d21cc8c1e
XiiaoYing <xiaoying96@hotmail.com>
2026-04-15 11:17:03 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Event Client App</h1>
+      <p>
+        Go to <a href="/register">/register</a> to register.
+      </p>
+    </div>
+  );
+}
