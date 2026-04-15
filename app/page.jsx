7be2f9853ce4395ab72733d1dcbdc78405ec813f
XiiaoYing <xiaoying96@hotmail.com>
2026-04-15 11:21:37 +0800

    Update and rename page.tsx to page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div style={{ padding: 20 }}>
       <h1>Event Client App</h1>
       <p>
-        Go to <a href="/register">/register</a> to register.
+        Please go to <a href="/register">/register</a> to register.
       </p>
     </div>
   );
